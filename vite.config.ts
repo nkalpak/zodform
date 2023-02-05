93694b4fce0b9c5163f418470a7e9a51dc979117
nkalpak <nikita.kalpakovski@outlook.com>
2023-02-05 13:53:13 +0100

    vite library build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,15 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/setup-tests.ts"],
+  },
+  build: {
+    lib: {
+      entry: path.resolve("src/index.ts"),
+      name: "@nkaplak/zodform",
+      fileName: "zodform",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
   },
 });
