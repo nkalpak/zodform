723edf72600c37a6364384f7a40442b4617f98c4
nkalpak <nikita.kalpakovski@outlook.com>
2023-02-08 17:00:01 +0100

    setup story in core

---
+++ apps/core/src/stories/form.stories.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Form, FormUiSchema, IObjectDefaultProps } from '../../dist';
+import { z } from 'zod';
+import { ObjectDefault } from '../components/default/object-default';
+import { EnumDefault } from '../components/default/enum-default';
+
+export function ConferenceRegistration() {
+  const PersonComponent = React.useCallback((props: IObjectDefaultProps) => {
+    return (
+      <ObjectDefault {...props}>
+        <div
+          style={{
+            display: 'grid',
+            gap: 8
+          }}
+        >
+          {props.children}
+        </div>
+      </ObjectDefault>
+    );
+  }, []);
+
+  const [schema] = React.useState(() =>
+    z.object({
+      people: z
+        .array(
+          z.object({
+            fullName: z.object({
+              firstName: z.string().min(1, 'First name is required'),
+              lastName: z.string().min(1, 'Last name is required')
+            }),
+            email: z.string().email().describe('myname@example.com'),
+            phoneNumber: z.string().describe('e.g. "+1 555 555 5555"')
+          })
+        )
+        .min(1, 'Please add at least one person'),
+
+      products: z.array(z.enum(['tShirt', 'coffeeCup'] as const)).min(1, 'Please select a product'),
+      paymentMethod: z.enum(['creditCard', 'payPal'] as const),
+
+      paypalNumber: z.string().optional()
+    })
+  );
+
+  const [uiSchema] = React.useState<FormUiSchema<typeof schema>>(() => ({
+    people: {
+      element: {
+        ui: {
+          title: 'Attendee',
+          component: PersonComponent
+        },
+        fullName: {
+          ui: {
+            component: ObjectDefault
+          },
+          firstName: {
+            label: 'First name'
+          },
+          lastName: {
+            label: 'Last name'
+          }
+        },
+        email: {
+          label: 'Email'
+        },
+        phoneNumber: {
+          label: 'Phone number'
+        }
+      }
+    },
+    products: {
+      title: 'Products',
+      optionLabels: {
+        coffeeCup: 'Coffee cup',
+        tShirt: 'T-shirt'
+      }
+    },
+    paymentMethod: {
+      component: EnumDefault,
+      label: 'Payment method',
+      optionLabels: {
+        creditCard: <span>💳 Credit card</span>,
+        payPal: <span>🐧 PayPal</span>
+      }
+    },
+    paypalNumber: {
+      label: 'PayPal number',
+      cond: (formData) => formData.paymentMethod === 'payPal'
+    }
+  }));
+
+  return (
+    <div
+      style={{
+        maxWidth: 500,
+        margin: 'auto'
+      }}
+    >
+      <Form
+        onSubmit={console.log}
+        title={<h1>Conference registration</h1>}
+        schema={schema}
+        uiSchema={uiSchema}
+      >
+        {() => {
+          return <button type="submit">Submit</button>;
+        }}
+      </Form>
+    </div>
+  );
+}
