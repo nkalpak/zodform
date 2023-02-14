415a0e8ef368b0441a19e9f2969d410919332c2e
nkalpak <nikita.kalpakovski@outlook.com>
2023-02-14 13:53:10 +0100

    update docs title

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -2,6 +2,11 @@ export default {
   logo: <span style={{ fontWeight: 'bold' }}>ZodForm</span>,
   project: {
     link: 'https://github.com/nkalpak/zodform'
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - ZodForm'
+    };
   }
   // ...
 };
