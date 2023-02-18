d9625ab7ddfe31c3c97e646515557fd318760542
nkalpak <nikita.kalpakovski@outlook.com>
2023-02-18 14:08:46 +0100

    fix required deep type

--- apps/core/src/utils/type-utils.ts
+++ apps/core/src/utils/type-utils.ts
@@ -2,12 +2,11 @@ import { UnionToIntersection } from 'type-fest';
 
 export type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true;
 export type IsNonUndefinedUnion<T> = IsUnion<Exclude<T, undefined | null>> extends true ? true : false;
+export type AnyFunction = (...args: any[]) => any;
 
 // Date is actually an object, but we don't want to recurse into it
-export type RequiredDeep<T> = T extends Date
+export type RequiredDeep<T> = T extends AnyFunction | Date | Array<any>
   ? T
   : T extends object
   ? { [K in keyof T]-?: RequiredDeep<T[K]> }
-  : T extends Array<infer U>
-  ? Array<RequiredDeep<U>>
   : T;
